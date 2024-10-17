c2aac66b8e2633f355cd88c594a3336ce5701cbc
LlyanaP <pollyanacostasousa@gmail.com>
2024-10-17 18:11:45 -0300

    quase finalizado

--- princess.sql
+++ princess.sql
@@ -9,3 +9,12 @@ INSERT INTO tbl_avalies (nome_avalie, createdAt, updatedAt) VALUES ('💙', now(
 UPDATE tbl_avalies
 SET nome_avalie = '💜💜💜💜💜', updatedAt = NOW()
 WHERE cod_avalie = 1;
+
+INSERT INTO tbl_cadastro_avalia (produto,marca,ean,utilizacaoCorreta,usoPessoal,dica, createdAt, updatedAt) VALUES ('Sabonete Facial Suavie','Darrow','3282770144369','Produto desemvolvido para as peles sensíveis, utilizado para limpeza facial, recomendado que o utilize ao lavar ao rosto duas vezes por dia','Utilizei Conforme a recomendação da minha médica, ele tem um bom sensorial e não sensibilizou a pele','Minha dica é que nao precisa esfregar muito a pele para limpar isso pode sensibilizar a pele ainda mais', now(), now());
+INSERT INTO tbl_cadastro_avalia (nome_avalie, createdAt, updatedAt) VALUES ('💙💙💙💙', now(), now());
+INSERT INTO tbl_cadastro_avalia (nome_avalie, createdAt, updatedAt) VALUES ('💙💙💙', now(), now());
+INSERT INTO tbl_cadastro_avalia (nome_avalie, createdAt, updatedAt) VALUES ('💙💙', now(), now());
+INSERT INTO tbl_cadastro_avalia (nome_avalie, createdAt, updatedAt) VALUES ('💙', now(), now());
+
+delete from tbl_cadastro_avalia
+WHERE cod_cadastro_avalia = 3;
--- routes/routeCadastroAvalia.js
+++ routes/routeCadastroAvalia.js
@@ -1,7 +1,68 @@
 const express = require('express');
 
-const modelAvalie = require('../modelo/cadastroAvaliação.js');
+const modelCadastroAvalie = require('../modelo/cadastroAvaliação.js');
 
 const router = express.Router();
 
 module.exports = router;
+
+router.get('/listagemDeProdutos',(req,res)=>{
+    modelCadastroAvalie.findAll()
+    .then(
+        (response)=>{
+            return res.status(201).json(
+                {
+                    errorStatus:false,
+                    mensageStatus:'Avaliação de Produto Aparece',
+                    data:response
+                }
+            );
+        }
+    )
+    .catch((error)=>{
+        return res.status(400).json(
+            {
+                errorStatus:true,
+                mensageStatus:'Houve um erro no avalia',
+                errorObject:error
+            }
+        );
+    });
+    
+});
+
+router.post('/cadastroAvalie', (req,res)=> {
+    let {produto, marca, ean, utilizacaoCorreta, usoPessoal, dica} = req.body;
+
+    modelCadastroAvalie.create(
+        {
+            produto,
+            marca,
+            ean,
+            utilizacaoCorreta,
+            usoPessoal,
+            dica
+        }
+    )
+    .then(
+        ()=>{
+            return res.status(201).json(
+                {
+                    errorStatus:false,
+                    mensageStatus: 'Avaliação do Produto Cadastrada com Sucesso'
+                    
+                }
+            );
+        }
+    )
+    .catch((error)=>{
+        return res.status(400).json(
+            {
+                errorStatus:true,
+                mensageStatus:'Falha no Cadastro da Avaliação',
+                errorObject:error
+            }
+        );
+    });
+    
+});
